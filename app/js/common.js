b9378bdcb171fd31fd00bd990f2665ffd64391f0
Kartel Maksym <kartmax1804@gmail.com>
2021-09-14 12:47:55 +0300

    page about

---
+++ app/js/common.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Custom JS
+
+    // preloader
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+
+
+})
+
+
+$(function() {
+
+    // Custom jQ
+
+    //js for carousel
+    $('.owl-carousel__list-items').owlCarousel({
+        loop: true,
+        margin: 20,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+
+    // owl carousel for prof-affil__items
+    $('.prof-affil__items').owlCarousel({
+        loop: false,
+        margin: 60,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0
+            },
+            400: {
+                items: 2,
+                margin: 20,
+            },
+            500: {
+                items: 3,
+                margin: 20,
+            },
+            600: {
+                items: 4,
+                margin: 30,
+            },
+            900: {
+                items: 6,
+                margin: 30,
+            },
+            1000: {
+                items: 6,
+                margin: 60,
+            }
+        }
+    })
+
+
+    //js for show success block
+    var $button = $('#send-btn'),
+        $containerForm = $('.modal-form-content'),
+        $containerSuccess = $('.modal-success-content');
+    $button.click('on', function() {
+        $containerForm.addClass('d-none');
+        $containerSuccess.removeClass('d-none');
+    })
+
+
+
+});
